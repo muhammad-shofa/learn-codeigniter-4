afa23eeb43934c5e3892a399d214408133f4f9e0
mshofadev <mshofa09@gmail.com>
2025-04-10 15:32:17 +0700

    Refactor: move navheader, sidebar, and footer to layout folder and include in template for cleaner structure

---
+++ integration-adminlte4-ci4/app/Views/layout/sidebar.php
@@ -0,0 +1,38 @@
+<!--begin::Sidebar Brand-->
+<div class="sidebar-brand">
+    <!--begin::Brand Link-->
+    <a href="./index.html" class="brand-link">
+        <!--begin::Brand Image-->
+        <img
+            src="../../dist/assets/img/AdminLTELogo.png"
+            alt="AdminLTE Logo"
+            class="brand-image opacity-75 shadow" />
+        <!--end::Brand Image-->
+        <!--begin::Brand Text-->
+        <span class="brand-text fw-light">Test Integration</span>
+        <!--end::Brand Text-->
+    </a>
+    <!--end::Brand Link-->
+</div>
+<!--end::Sidebar Brand-->
+<!--begin::Sidebar Wrapper-->
+<div class="sidebar-wrapper">
+    <nav class="mt-2">
+        <!--begin::Sidebar Menu-->
+        <ul
+            class="nav sidebar-menu flex-column"
+            data-lte-toggle="treeview"
+            role="menu"
+            data-accordion="false">
+            <li class="nav-item">
+                <a href="#0" class="nav-link">
+                    <i class="nav-icon bi bi-palette"></i>
+                    <p>Dashboard</p>
+                </a>
+            </li>
+            
+        </ul>
+        <!--end::Sidebar Menu-->
+    </nav>
+</div>
+<!--end::Sidebar Wrapper-->
